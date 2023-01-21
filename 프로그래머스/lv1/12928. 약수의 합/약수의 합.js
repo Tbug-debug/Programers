c8aa91a22ac3f2ba906104c559f44563330209c5
Tbug-debug <76459231+Tbug-debug@users.noreply.github.com>
2023-01-21 12:41:50 +0900

    [level 1] Title: 약수의 합, Time: 0.26 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12928. 약수의 합/약수의 합.js
@@ -0,0 +1,11 @@
+function solution(n) {
+ let arr = [];
+        for (let i = 0; i <= n; i++) {
+             if (n === 0) return 0;
+            let devied = n / i;
+            if (Number.isInteger(devied)) {
+                arr.push(n / i);
+            }
+        }
+        return arr.reduce((a, b) => a + b);
+}
