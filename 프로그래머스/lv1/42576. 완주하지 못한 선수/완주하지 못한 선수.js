b767f3427ca39912857e7fe3b35fb299a032e411
Tbug-debug <76459231+Tbug-debug@users.noreply.github.com>
2023-01-23 00:57:31 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 101.44 ms, Memory: 56.9 MB -BaekjoonHub

--- 프로그래머스/lv1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
+++ 프로그래머스/lv1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
@@ -1,10 +1,10 @@
    function solution(participant, completion) {
-                let par = participant.sort();
-                let com = completion.sort();
+   participant.sort();
+    completion.sort();
 
-                for (let i = 0; i < participant.length; i++) {
-                    if (participant[i] !== completion[i]) {
-                        return participant[i]
-                    }
-                }
-            }
+    for (let i = 0; i < participant.length; i++) {
+        if (participant[i] !== completion[i]) {
+            return participant[i]
+        }
+    }
+}
