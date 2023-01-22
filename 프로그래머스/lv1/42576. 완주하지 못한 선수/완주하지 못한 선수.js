77a82afd8447d325adbe0cb493122e3188d378a7
Tbug-debug <76459231+Tbug-debug@users.noreply.github.com>
2023-01-23 01:36:59 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 101.22 ms, Memory: 56.9 MB -BaekjoonHub

--- 프로그래머스/lv1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
+++ 프로그래머스/lv1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
@@ -1,10 +1,10 @@
-   function solution(participant, completion) {
-   participant.sort();
-    completion.sort();
+            function solution(participant, completion) {
+                participant.sort();
+                completion.sort();
 
-    for (let i = 0; i < participant.length; i++) {
-        if (participant[i] !== completion[i]) {
-            return participant[i]
-        }
-    }
-}
+                for (let i = 0; i < participant.length; i++) {
+                    if (participant[i] !== completion[i]) {
+                        return participant[i];
+                    }
+                }
+            }
