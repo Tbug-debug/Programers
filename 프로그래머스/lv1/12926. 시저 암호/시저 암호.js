71eb9fa4d8caac6eaefec770b8e498c06610443e
Tbug-debug <76459231+Tbug-debug@users.noreply.github.com>
2023-01-29 16:56:00 +0900

    [level 1] Title: 시저 암호, Time: 2.92 ms, Memory: 34.2 MB -BaekjoonHub

--- 프로그래머스/lv1/12926. 시저 암호/시저 암호.js
+++ 프로그래머스/lv1/12926. 시저 암호/시저 암호.js
@@ -3,34 +3,34 @@ function solution(s, n) {
                 let str = s.split(' ');
                 var blank_pattern = /[\s]/g;
                 let answer = '';
-                if (blank_pattern.test(s) == true) {
+                if (blank_pattern.test(s) == true) { // 공백이 있을 경우,
                     for (let i = 0; i < s.length; i++) {
-                        if (s[i] !== s[i].toUpperCase()) {
-                            if (s.charCodeAt(i) + n >= 123) {
+                        if (s[i] !== s[i].toUpperCase()) { //대문자인 경우
+                            if (s.charCodeAt(i) + n >= 123) { // 아스키코드가 123이 넘어가는 경우
                                 arr.push(s[i].charCodeAt(0) + n - 25 - 1);
-                            } else {
+                            } else { // 안넘어가는 경우
                                 arr.push(s[i].charCodeAt(0) + n);
                             }
-                        } else {
-                            if (s.charCodeAt(i) + n >= 91) {
+                        } else { // 소문자인 경우
+                            if (s.charCodeAt(i) + n >= 91) {// 아스키코드가 91이 넘어가는 경우
                                 arr.push(s[i].charCodeAt(0) + n - 25 - 1);
-                            } else {
+                            } else { // 안넘어가는 경우
                                 arr.push(s[i].charCodeAt(0) + n);
                             }
                         }
                     }
-                } else {
+                } else { // 공백이 없는 경우,
                     for (let i = 0; i < s.length; i++) {
-                        if (s[i] !== s[i].toUpperCase()) {
-                            if (s.charCodeAt(i) + n >= 123) {
+                        if (s[i] !== s[i].toUpperCase()) { //대문자인 경우
+                            if (s.charCodeAt(i) + n >= 123) { // 아스키코드가 123이 넘어가는 경우
                                 arr.push(s.charCodeAt(i) + n - 25 - 1);
-                            } else {
+                            } else { // 안넘어가는 경우
                                 arr.push(s.charCodeAt(i) + n);
                             }
-                        } else {
-                            if (s.charCodeAt(i) + n >= 91) {
+                        } else { // 소문자인 경우
+                            if (s.charCodeAt(i) + n >= 91) { // 아스키코드가 91이 넘어가는 경우
                                 arr.push(s.charCodeAt(i) + n - 25 - 1);
-                            } else {
+                            } else { // 안넘어가는 경우
                                 arr.push(s.charCodeAt(i) + n);
                             }
                         }
