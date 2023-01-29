b1a0a5a06fd48ca8675b6b9c4d145b995dc95f7e
Tbug-debug <76459231+Tbug-debug@users.noreply.github.com>
2023-01-29 17:11:52 +0900

    [level 1] Title: 로또의 최고 순위와 최저 순위, Time: 0.19 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv1/77484. 로또의 최고 순위와 최저 순위/로또의 최고 순위와 최저 순위.js
+++ 프로그래머스/lv1/77484. 로또의 최고 순위와 최저 순위/로또의 최고 순위와 최저 순위.js
@@ -5,28 +5,29 @@ function solution(lottos, win_nums) {
     let rank = { 1: 6, 2: 5, 3: 4, 4: 3, 5: 2 };
 
     function makeRank(obj, value) {
-        return Object.keys(obj).find((key) => obj[key] === value);
+        return Object.keys(obj).find((key) => obj[key] === value); //오브젝트의 키를 통해서 value와 같을시에 key를 반환한다 
     }
 
     lottos.forEach((a) => {
         if (a == 0) {
-            zero++;
+            zero++; // 0일때는 zero 카운트를 올려준다.
         }
     });
 
     for (let i = 0; i < lottos.length; i++) {
         for (let j = 0; j < lottos.length; j++) {
             if (lottos[i] === win_nums[j]) {
-                count++;
+                count++; // 로또의 당첨번호와 로또 구매자 번호가 일치할 경우 count를 올려준다.
             }
         }
     }
 
-    let max = Math.max(count + zero);
-    let min = Math.max(count);
+    let max = count + zero; // zero와 count의 값을 더하면 최고 순위가 나온다.
+    let min = count; //count만 할 경우 최저 순위가 나온다. 
 
-    let maxrank = makeRank(rank, max);
-    let minrank = makeRank(rank, min);
+    let maxrank = makeRank(rank, max); // 최고순위를 makeRank에 넣고 반환
+    let minrank = makeRank(rank, min); // 최저순위를 minrank에 넣고 반환
+    
     answer.push(maxrank == undefined ? 6 : Number(maxrank), minrank == undefined ? 6 : Number(minrank));
     return answer;
 }
