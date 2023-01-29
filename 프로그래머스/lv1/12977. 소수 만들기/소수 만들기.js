d785bec6a74b7c40e34290fd4280a30b50dc4ffe
Tbug-debug <76459231+Tbug-debug@users.noreply.github.com>
2023-01-29 16:47:33 +0900

    [level 1] Title: 소수 만들기, Time: 0.09 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv1/12977. 소수 만들기/소수 만들기.js
+++ 프로그래머스/lv1/12977. 소수 만들기/소수 만들기.js
@@ -5,20 +5,21 @@ function solution(num) {
     for (let i = 0; i < num.length; i++) {
         for (let j = i + 1; j < num.length; j++) {
             for (let k = j + 1; k < num.length; k++) {
-                arr.push(num[i] + num[j] + num[k]);
+                arr.push(num[i] + num[j] + num[k]); // 3중 for문으로 3개의 숫자를 더한다.
             }
         }
     }
 
-    let kum = arr.forEach((num) => {
+    arr.forEach((num) => { // 3개의 값을 더한 배열을 forEach를 돌려 소수인 것만 answer +1을 한다.
         function isPrime(num) {
             for (let i = 2; i <= Math.sqrt(num); i++) {
-                if (num % i === 0) return false;
+                if (num % i === 0) return false; // 소수가 아닌건 false로 반환한다.
             }
-            return num >= 2;
+            // 소수는 1보다 큰 정수이다
+            return num > 1;
         }
 
-        if (isPrime(num)) answer += 1;
+        if (isPrime(num)) answer += 1; // 소수일 경우 answer에 1을 더해준다.
     });
 
     return answer;
