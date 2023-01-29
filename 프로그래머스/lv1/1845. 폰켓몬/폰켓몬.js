016f738a479bce032e8d58caae1dc4da3b052409
Tbug-debug <76459231+Tbug-debug@users.noreply.github.com>
2023-01-29 17:07:21 +0900

    [level 1] Title: 폰켓몬, Time: 0.75 ms, Memory: 34 MB -BaekjoonHub

--- 프로그래머스/lv1/1845. 폰켓몬/폰켓몬.js
+++ 프로그래머스/lv1/1845. 폰켓몬/폰켓몬.js
@@ -1,15 +1,15 @@
 function solution(nums) {
 
 
-    let length = nums.length / 2;
+    let length = nums.length / 2; // N/2를 한다.
 
-    let arr = nums.filter((element, index) => {
+    let arr = nums.filter((element, index) => { // nums로 받는 배열 안에 같은 원소를 다 제거한다.
         return nums.indexOf(element) === index;
     });
 
-    if (arr.length >= length) {
-        return length
-    } else if (arr.length < length) {
-        return arr.length
+    if (arr.length >= length) { // 만약 같은 값을 제거한 배열의 length가 N/2한 것보다 클 경우,
+        return length // N/2가 정답.
+    } else if (arr.length < length) { // 그 반대일경우,
+        return arr.length // arr이 정답
     }
 }
