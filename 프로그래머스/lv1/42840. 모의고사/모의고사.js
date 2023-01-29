4cb10490260d1c5fe113b114f671391ec0032b00
Tbug-debug <76459231+Tbug-debug@users.noreply.github.com>
2023-01-29 16:39:25 +0900

    [level 1] Title: 모의고사, Time: 2.86 ms, Memory: 36.8 MB -BaekjoonHub

--- 프로그래머스/lv1/42840. 모의고사/모의고사.js
+++ 프로그래머스/lv1/42840. 모의고사/모의고사.js
@@ -1,26 +1,31 @@
 function solution(answers) {
+     const supoja = [
+                    [1, 2, 3, 4, 5],
+                    [2, 1, 2, 3, 2, 4, 2, 5],
+                    [3, 3, 1, 1, 2, 2, 4, 4, 5, 5],
+                ];
+                const supojaLength = [supoja[0].length, supoja[1].length, supoja[2].length];
 
+                const num = [0, 0, 0];
+                const answer = [];
 
-    const supoja = [
-        [1, 2, 3, 4, 5],
-        [2, 1, 2, 3, 2, 4, 2, 5],
-        [3, 3, 1, 1, 2, 2, 4, 4, 5, 5],
-    ];
-    const num = [0, 0, 0];
-    const answer = [];
+                for (let i = 0; i < answers.length; i++) {
+                    for (let j = 0; j < supojaLength.length; j++) {
+                        if (answers[i] === supoja[j][i % supojaLength[j]]) num[j]++;
+                        // answers[0] === supoja[0][0]
+                        // answers[0] === supoja[0][1]
+                        // answers[0] === supoja[0][2]
+                        // 위와 같이 비교하여 서로 같은 값일떄 배열에 count를 + 해준다.
+                    }
+                }
 
-    for (let i = 0; i < answers.length; i++) {
-        for (let j = 0; j < supoja.length; j++) {
-            if (answers[i] === supoja[j][i % supoja[j].length]) num[j]++;
-        }
-    }
+                const maxNum = Math.max(...num); // num배열의 최고값을 구한다.
+    
+                num.forEach((a, i) => {
+                    if (a === maxNum) { //a와 maxNum의 값을 비교하여 같으면 answer배열에 +1씩 해준다.
+                        answer.push(i + 1); 
+                    }
+                });
 
-    const maxNum = Math.max(...num);
-    num.forEach((a, i) => {
-        if (a === maxNum) {
-            answer.push(i + 1);
-        }
-    });
-
-    return answer;
+             return answer;
 }
