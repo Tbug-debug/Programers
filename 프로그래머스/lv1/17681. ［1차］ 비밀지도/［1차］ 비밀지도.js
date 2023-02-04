f0e7d8b0ee3b89234140e61f1d72287792d191df
Tbug-debug <76459231+Tbug-debug@users.noreply.github.com>
2023-02-05 01:32:29 +0900

    [level 1] Title: [1차] 비밀지도, Time: 0.08 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.js
+++ 프로그래머스/lv1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.js
@@ -5,23 +5,22 @@
 
                 for (let i = 0; i < arr1.length; i++) {
                     arr1ten.push(arr1[i].toString(2));
-                    arr2ten.push(arr2[i].toString(2));
+                    arr2ten.push(arr2[i].toString(2)); // 10진수를 2진수로 전환
                     if (arr1ten[i].length < n) {
                         arr1ten[i] = '0'.repeat(n - arr1ten[i].length) + arr1ten[i];
                     }
                     if (arr2ten[i].length < n) {
-                        arr2ten[i] = '0'.repeat(n - arr2ten[i].length) + arr2ten[i];
+                        arr2ten[i] = '0'.repeat(n - arr2ten[i].length) + arr2ten[i]; // 만약 2진수 전환 과정에서 n보다 작으면 부족한 만큼 0을 추가해준다.
                     }
                 }
 
-                console.log(arr1ten, arr2ten);
 
-                for (let i = 0; i < n; i++) {
+                   for (let i = 0; i < n; i++) {
                     let board = '';
                     for (let j = 0; j < n; j++) {
-                        if (Number(arr1ten[i][j]) || Number(arr2ten[i][j])) {
+                        if (Number(arr1ten[i][j]) == 1 || Number(arr2ten[i][j]) == 1) { // 지도 2개의 겹치는 부분 중에 하나라도 1일 경우 '#'
                             board += '#';
-                        } else {
+                        } else { // 1이 아닐 경우, 0일 경우 공백을 넣어주었다.
                             board += ' ';
                         }
                     }
