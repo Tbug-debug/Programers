c0e6f1c3a0e0f5f84ecd6c5c5da07745636fa39f
Tbug-debug <76459231+Tbug-debug@users.noreply.github.com>
2023-02-21 23:48:42 +0900

    [level 0] Title: 영어가 싫어요, Time: 0.15 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120894. 영어가 싫어요/영어가 싫어요.js
@@ -0,0 +1,12 @@
+function solution(numbers) {
+    const obj = {
+        zero: 0, one: 1, two: 2, three: 3, four: 4,
+        five: 5, six: 6, seven: 7, eight: 8, nine: 9
+    };
+
+    const num = numbers.replace(/zero|one|two|three|four|five|six|seven|eight|nine/g, (v) => {
+        return obj[v];
+    });
+
+    return Number(num);
+}
